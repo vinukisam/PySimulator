1164c59be018551bfd629dd502e16540e4175ad0
Thulasee Shan <thualsee_s@hotmail.com>
2024-09-24 23:05:10 -0700

    image-captured

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import random
 import socket
 import threading
-from time import sleep
+#from pygame_screen_record import ScreenRecorder
 
 # Initialize Pygame
 pygame.init()
@@ -23,9 +23,9 @@ class Arm:
     def __init__(self):
         self.width = 100
         self.x = SCREEN_WIDTH // 2 - self.width // 2
-        self.y = SCREEN_HEIGHT // 2 - 160
+        self.y = SCREEN_HEIGHT // 2 - 180
         self.height = 20
-        self.color = (0, 0, 255)
+        self.color = (120,120,120)
         self.active = False
         self.speed = 0
         self.acceleration = 2
@@ -34,6 +34,9 @@ class Arm:
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, [self.x, self.y, self.width, self.height])
+        pygame.draw.rect(screen, self.color, [self.x + (self.width//2 - self.height//2), 
+                                              self.y - self.width//2, 
+                                              self.height, self.width//2])
 
     def move(self):
         if self.active:
@@ -42,7 +45,7 @@ class Arm:
             if self.y > SCREEN_HEIGHT // 2 + 150:  # Move the arm to the right
                 self.active = False
                 self.speed = 0
-                self.y = SCREEN_HEIGHT // 2 - 160  # Reset arm position
+                self.y = SCREEN_HEIGHT // 2 - 180  # Reset arm position
 
     def push(self, cookies):
         if self.active:
@@ -61,22 +64,33 @@ class Cookie:
         self.width = 50
         self.height = 50
         self.dead = False
-        self.bad = random.randint(0,100)
+        self.bad = random.randint(0,10)
         self.isBad = 0<self.bad<4
 
         if self.bad == 1 or self.bad == 3:
             #self.color = (139,69,19)
-            self.color = (random.randint(90,150), random.randint(0,75) , random.randint(0,30))
+            bad_colors = [(40,18,5), (70,20,15), (120,80,80)]
+            self.color = bad_colors[random.randint(0,2)]
         else:
             self.color = BROWN
+
+        bad_shape = [(0,0,0), (1,2,5), (3,4,5), (4,5,5)]
+        if self.bad == 2 or self.bad ==3:
+            self.shape_arc = bad_shape[random.randint(1,3)]
+        else:
+            self.shape_arc = bad_shape[0]
    
     def draw(self, screen):
         # Draw the full cookie
-        pygame.draw.ellipse(screen, self.color, [self.x, self.y, self.width, self.height])
+        pygame.draw.ellipse(screen, 
+                            self.color, 
+                            [self.x, self.y, self.width, self.height])
 
         # Draw the bite (an arc)
-        if self.bad == 2 or self.bad == 3:
-            pygame.draw.arc(screen, (0,0,0), [self.x, self.y, self.width, self.height], 0, 1.5, 5)            
+        pygame.draw.arc(screen,
+                        (0,0,0), 
+                        [self.x, self.y, self.width, self.height],
+                        self.shape_arc[0], self.shape_arc[1], self.shape_arc[2])
 
     def move(self, speed):
         if(self.dead == False) :
@@ -90,6 +104,19 @@ class Belt:
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, [0, SCREEN_HEIGHT // 2 - self.height//2, SCREEN_WIDTH, self.height])
 
+class Camera:
+    def __init__(self):
+        self.capture_area = pygame.Rect(SCREEN_WIDTH // 2 - 155, SCREEN_HEIGHT // 2 - 150, 68, 300)
+        self.cam_poly = [(self.capture_area.left + 10, self.capture_area.top - 10),
+                         (self.capture_area.left + 20, self.capture_area.top - 30),
+                         (self.capture_area.left + 40, self.capture_area.top - 30),
+                         (self.capture_area.left + 50, self.capture_area.top - 10)]
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (200,0,0), self.capture_area, 2)
+        pygame.draw.polygon(screen, (100,100,100), self.cam_poly)
+
+
 def main():
     clock = pygame.time.Clock()
     cookies = []
@@ -99,7 +126,9 @@ def main():
     running = True
     stX = 0
     badCookies = 0
+    imgCounter = -2
 
+    #-------------------------------------------------------------------------------------
     # Set up the socket server
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('localhost', 11999))
@@ -111,6 +140,18 @@ def main():
     server_thread = threading.Thread(target=start_server, args=(server, stop_event))
     server_thread.daemon = True
     server_thread.start()
+    #-------------------------------------------------------------------------------------
+
+    #-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+    # Initialize the screen recorder
+    # Capture the defined area
+    camera = Camera()
+    #capture_surface = screen.subsurface(camera.capture_area)
+    #recorder.capture_frame(capture_surface)
+    #recorder = ScreenRecorder(60, capture_surface)  # 60 FPS
+    #recorder.start_rec()  # Start recording
+    
+    #-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
     while running:
         for event in pygame.event.get():
@@ -125,7 +166,7 @@ def main():
         #    cookies.append(Cookie(0, SCREEN_HEIGHT // 2 - 25))
             stY = SCREEN_HEIGHT // 2 - (100 + random.randint(0, 45))
             while stY <= SCREEN_HEIGHT // 2 + 100:
-                cookie = Cookie(stX, stY)
+                cookie = Cookie(random.randint(0, 10), stY)
                 #increment bad cookie counter
                 if cookie.isBad:
                     badCookies += 1
@@ -146,7 +187,7 @@ def main():
         
         #Move the X pointer
         stX += speed
-        if(stX > 50):
+        if(stX > 60):
             stX = 0
 
         # Remove cookies that have moved off the screen
@@ -158,12 +199,37 @@ def main():
         # Blit the text onto the screen
         screen.blit(text, (0, 0))  # Position the text at (100, 100)
 
+        camera.draw(screen)
+        #-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+        # Capture the defined area
+        #capture_surface = screen.subsurface(camera.capture_area)
+        #recorder.capture_frame(capture_surface)
+        if stX == 0:
+            if imgCounter>0:
+                capture_surface = screen.subsurface(camera.capture_area)
+                capture_image = pygame.Surface(camera.capture_area.size)
+                capture_image.blit(capture_surface, (0, 0))
+                # Save the captured image
+                pygame.image.save(capture_image, f"captured_image{imgCounter}.png")
+            imgCounter += 1
+        #-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
         pygame.display.flip()
         clock.tick(60)
 
+    try:
+        pass
+        #-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+        #recorder.stop_rec()  # Stop recording
+        #recorder.save_recording("partial_recording.mp4")  # Save the recording
+        # Capture the defined area
+        #-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+    except Exception as e:
+        print(f"Error in worker thread: {e}")
+        
     pygame.quit()
-    
 
+#-------------------------------------------------------------------------------------
 # Function to handle incoming socket connections
 def handle_client(client_socket, stop_event):
     while not stop_event.is_set():
@@ -179,10 +245,14 @@ def handle_client(client_socket, stop_event):
 # Start a thread to handle incoming connections
 def start_server(server, stop_event):
     while not stop_event.is_set():
-        client_socket, addr = server.accept()
-        client_handler = threading.Thread(target=handle_client, args=(client_socket, stop_event))
-        client_handler.daemon = True
-        client_handler.start()
+        try:
+            client_socket, addr = server.accept()
+            client_handler = threading.Thread(target=handle_client, args=(client_socket, stop_event))
+            client_handler.daemon = True
+            client_handler.start()
+        except:
+            break
+#-------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     main()
